5e112d34acbbb0ce233d3cb57931b519431b413c
“robmonday” <10903531+robmonday@users.noreply.github.com>
2022-12-30 15:33:46 -0500

    lesson 5c exercises through 'Test Coverage' heading

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -22,7 +22,11 @@ const NoteForm = ({ createNote }) => {
       <h2>Create a new note</h2>
 
       <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleChange}/>
+        <input
+          value={newNote}
+          onChange={handleChange}
+          placeholder='write here note content'
+        />
         <button type="submit">save</button>
       </form>
     </div>
